27ab5c217dc4b6d3ff88fc7962461654e66f8758
Yang09701194 <mustnecessity@gmail.com>
2020-07-24 21:44:36 +0800

    add unit test function and run

---
+++ UnitTestPractice/UnitTestPractice/WaterBottles.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestPractice
+{
+	public class WaterBottles
+	{
+
+		// mouse right click > top create unit tests
+		public int NumWaterBottles(int numBottles, int numExchange)
+		{
+			int totalDrinkBottles = numBottles;
+			return Drink(totalDrinkBottles, totalDrinkBottles, numExchange);
+		}
+
+		private int Drink(int totalDrinkBottles, int emptyBottles, int numExchange)
+		{
+			int exchangeRemainEmpty = emptyBottles % numExchange;
+			int exchange = emptyBottles / numExchange;
+			if (exchange == 0)
+				return totalDrinkBottles;
+
+			emptyBottles = exchangeRemainEmpty + exchange;
+			totalDrinkBottles += exchange;
+
+			return Drink(totalDrinkBottles, emptyBottles, numExchange);
+		}
+
+	}
+}
---
+++ UnitTestPractice/UnitTestPracticeTests/WaterBottlesTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestPractice;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestPractice.Tests
+{
+	[TestClass()]
+	public class WaterBottlesTests
+	{
+		// Menu > Test Explorer   或右鍵新增Test專案  還有左邊三種run和方法上右鍵run
+		[TestMethod()]
+		[Timeout(2000)]  // Milliseconds
+		public void NumWaterBottlesTest()
+		{
+			// arrange
+			int numBottles = 9, numExchange = 3;
+			int expected = 13;
+
+			// act
+			WaterBottles waterBottles = new WaterBottles();
+			int result = waterBottles.NumWaterBottles(numBottles, numExchange);
+			
+			// assert
+			Assert.AreEqual(expected, result);
+
+		}
+	}
+}
